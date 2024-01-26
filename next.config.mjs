ad8e4009b84c421a48eade833a643bde36748f6f
Prince Osei-Akyeampong <pokax2000@gmail.com>
2024-01-26 16:39:29 -0500

    build output

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   output: 'export',
+  distDir: 'build',
+  assetPrefix: '/next-js-todos-app/',
+  basePath: '/next-js-todos-ui'
 };
 
 export default nextConfig;
